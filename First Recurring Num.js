7abf2cb2303c6d7550456568bb7dd981cdd9d6c7
VladNCode <vldsmrfow@gmail.com>
2022-01-01 09:46:58 +0200

    added bonus question solution

--- First Recurring Num.js
+++ First Recurring Num.js
@@ -1,7 +1,3 @@
-const arr1 = [2, 5, 1, 2, 3, 5, 1, 2, 4];
-const arr2 = [2, 1, 1, 2, 3, 4, 5, 1, 2, 4];
-const arr3 = [2, 3, 4, 5];
-
 const findFirstReNum = function (arr) {
   const set = new Set();
 
@@ -23,7 +19,7 @@ const findFirstReNum2 = function (arr) {
   }
 
   return undefined;
-};
+}; // O(n)
 
 const findFirstReNum3 = function (arr) {
   for (let i = 0; i < arr.length; i++) {
@@ -32,12 +28,37 @@ const findFirstReNum3 = function (arr) {
     }
   }
   return undefined;
+}; // O(n^2)
+
+const bonus = function (arr) {
+  let x = Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j] && j < x) x = j;
+    }
+  }
+
+  return x === Infinity ? undefined : arr[x];
 };
 
-// console.log(findFirstReNum(arr1));
-// console.log(findFirstReNum(arr2));
-// console.log(findFirstReNum(arr3));
+const bonus2 = function (arr) {
+  let x = Infinity;
+  const obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[arr[i]] && i < x) x = i;
+    obj[arr[i]] = true;
+  }
 
-console.log(findFirstReNum2(arr1));
-// console.log(findFirstReNum2(arr2));
-// console.log(findFirstReNum2(arr3));
+  return x === Infinity ? undefined : arr[x];
+};
+
+const arr1 = [2, 5, 1, 2, 3, 5, 1, 2, 4];
+const arr2 = [2, 1, 1, 2, 3, 4, 5, 1, 2, 4];
+const arr3 = [2, 3, 4, 5];
+const arr4 = [2, 5, 5, 2, 3, 5, 1, 2, 4];
+const arr5 = [2, 4, 3, 7, 8, 8, 1, 2, 4];
+const arr6 = [2, 4, 3, 2, 8, 8, 1, 2, 4];
+const arr7 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(bonus2(arr7));
