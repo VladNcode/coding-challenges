61d2a3f9b324edf63fcf90e7005074f34b4dbb82
VladNCode <vldsmrfow@gmail.com>
2022-07-19 11:59:57 +0300

    object

---
+++ ObjectParse/object.js
@@ -0,0 +1,22 @@
+const data = ['fizz = 3', 'buzz.one = item1', 'buzz.one = item2', 'buzz.one.two = 123'];
+// const data = ['fizzbuzz.root.a.b.c = 13'];
+
+let fin = {};
+
+// {
+//   "fizz": 3,
+//   "buzz": {
+//     "one": {
+//       "0": "item1",
+//       "1": "item2",
+//       "two": 123
+//     }
+//   },
+//   "fizzbuzz": {
+//     "root": {
+//       "a": {
+//         "c": 13
+//       }
+//     }
+//   }
+// }
