392e13756531b045263c0cdc27e6b02af2c33936
Vladyslav Nikiforov <vladncode@gmail.com>
2022-07-23 18:11:48 +0300

    testing bind

--- JavaScript/currying.js
+++ JavaScript/currying.js
@@ -1,6 +1,16 @@
-const a =
-	(...args) =>
-	(...bargs) =>
-		bargs.length ? a(...args, ...bargs) : args.reduce((acc, num) => acc + (typeof num === 'number' ? num : 0), 0);
+// const a =
+// 	(...args) =>
+// 	(...bargs) =>
+// 		bargs.length ? a(...args, ...bargs) : args.reduce((acc, num) => acc + (typeof num === 'number' ? num : 0), 0)
+
+let counter = 0;
+const a = (...args) => {
+	counter++;
+	return (...bargs) =>
+		bargs.length ? a.bind(...args, ...bargs) : args.reduce((acc, num) => acc + (typeof num === 'number' ? num : 0), 0);
+};
 
 console.log(a(1, 2, -3)(4)(2)(9, -9)(1, 1, 1, 1)(5, 'hello')(0)());
+console.log('counter :>> ', counter);
+
+// bind === 4, invoke === 7
