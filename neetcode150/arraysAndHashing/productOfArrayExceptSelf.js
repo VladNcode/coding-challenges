f1259c7e7072e65e6f734df8794ba37e673140f7
vladncode <vladncode@gmail.com>
2023-01-06 20:39:57 +0200

    product of array except self with no division

--- neetcode150/arraysAndHashing/productOfArrayExceptSelf.js
+++ neetcode150/arraysAndHashing/productOfArrayExceptSelf.js
@@ -1,25 +1,42 @@
-// TODO: Should be done without division
+// const productExceptSelf = nums => {
+// 	const zeroes = nums.filter(num => !num);
+
+// 	if (zeroes.length > 1) {
+// 		return new Array(nums.length).fill(0);
+// 	}
+
+// 	const total = nums.reduce((acc, el) => {
+// 		return el === 0 ? acc : acc * el;
+// 	}, 1);
+
+// 	console.log(total);
+
+// 	return nums.map(num => {
+// 		if (zeroes.length) {
+// 			return !num ? total : 0;
+// 		}
+
+// 		return total / num;
+// 	});
+// };
 
 const productExceptSelf = nums => {
-	const zeroes = nums.filter(num => !num);
+	const result = new Array(nums.length).fill(1);
 
-	if (zeroes.length > 1) {
-		return new Array(nums.length).fill(0);
+	let prefix = 1;
+	let postfix = 1;
+
+	for (let i = 0; i < nums.length; i++) {
+		result[i] = prefix;
+		prefix *= nums[i];
 	}
 
-	const total = nums.reduce((acc, el) => {
-		return el === 0 ? acc : acc * el;
-	}, 1);
+	for (let i = nums.length - 2; i >= 0; i--) {
+		postfix *= nums[i + 1];
+		result[i] *= postfix;
+	}
 
-	console.log(total);
-
-	return nums.map(num => {
-		if (zeroes.length) {
-			return !num ? total : 0;
-		}
-
-		return total / num;
-	});
+	return result;
 };
 
 const nums = [1, 2, 3, 4];
