37cf1d2bc4ddadb6681c5fa7bb42dc592cd10feb
Vladyslav Nikiforov <91598576+VladNcode@users.noreply.github.com>
2023-05-19 20:46:34 +0300

    Create day15.js

---
+++ JavaScript/LeetCode30DaysChallenge/day15.js
@@ -0,0 +1,11 @@
+const debounce = (fn, t) => {
+  let timeout;
+
+  return function (...args) {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      fn(...args);
+    }, t);
+  };
+};
