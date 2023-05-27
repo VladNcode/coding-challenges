14fdb8374b0acc8a321456b8fb6edaf5978b7345
vladncode <vladncode@gmail.com>
2023-05-27 11:03:21 +0300

    Day 17

--- JavaScript/LeetCode30DaysChallenge/day17.js
+++ JavaScript/LeetCode30DaysChallenge/day17.js
@@ -5,7 +5,7 @@ const areDeeplyEqual = (o1, o2) => {
 	// All equal values and same objects are eliminated
 	if (o1 === o2) return true;
 
-	// If o1 !== o2 mean we either have object || array || values of the same type that are not equal
+	// If o1 !== o2 we either have object || array || values of the same type that are not equal
 	// Check if they are objects or arrays, if its not the case than we have non equal values
 	if (typeof o1 !== 'object' || typeof o2 !== 'object') return false;
 	if (Array.isArray(o1) !== Array.isArray(o2)) return false;
